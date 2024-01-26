80ad36e56dc008825b37d7d042c689ddc5ca696e
zGiusee <giusebelforte@gmail.com>
2024-01-26 10:04:54 +0100

    [UPDATE] removed code parts in header.php and footer.php

--- header.php
+++ header.php
@@ -2,29 +2,12 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php-hotel</title>
-    <!-- Main style -->
-    <link rel="stylesheet" href="./style/style.css">
-    <!-- Cdn's -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
-
-<body>
-    <header>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <h1 class="fw-bold py-4">Blookings</h1>
-                </div>
+<header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="fw-bold py-4">Blookings</h1>
             </div>
         </div>
-    </header>
-</body>
-
-</html>
+    </div>
+</header>
